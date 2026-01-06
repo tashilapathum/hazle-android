870d43d854b66397216a60eafddf694b89f273e0
Tashila Pathum <tashila.pathum@gmail.com>
2026-01-07 00:29:07 +0530

    Release v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
