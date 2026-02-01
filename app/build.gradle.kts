85bb34653ea43cd1d6d950f2a9f685dd26e98350
Tashila Pathum <tashila.pathum@gmail.com>
2026-02-01 09:23:09 +0530

    Release v0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tashila.hazle"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 4
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
