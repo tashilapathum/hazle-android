fb915eef3bd44732fc0eb6271f542dc02ee967da
Tashila Pathum <tashila.pathum@gmail.com>
2026-02-01 13:27:27 +0530

    Improve debug build configuration
    This commit enhances the debug build process by:
    *   Adding a separate debug manifest to enable cleartext traffic, allowing connections to local servers.
    *   Introducing a network security configuration to explicitly permit cleartext traffic for local debugging IPs.
    *   Appending a `.debug` suffix to the application ID for debug builds.
    *   Setting a "Hazle Debug" app name for debug variants.

--- app/src/main/java/com/tashila/hazle/utils/Constants.kt
+++ app/src/main/java/com/tashila/hazle/utils/Constants.kt
@@ -8,7 +8,6 @@ else
     "https://api.hazle.tashila.me/"
 
 // "http://10.0.2.2:8080/" // For Android Emulator
-// "http://192.168.100.80:8080/" // Hutch wifi
-// "http://192.168.0.101:8080/" // Dialog wifi
+// "http://192.168.1.115:8080/" // Local deploy
 // "https://hazle.onrender.com/" // Render deploy
 // "https://api.hazle.tashila.me/" // Live deploy
