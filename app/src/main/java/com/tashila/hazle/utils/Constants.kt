48868cbfe269db736362c68c461cd6581a030088
Tashila Pathum <tashila.pathum@gmail.com>
2026-02-07 12:26:17 +0530

    Pointed dev to Railway deployment

--- app/src/main/java/com/tashila/hazle/utils/Constants.kt
+++ app/src/main/java/com/tashila/hazle/utils/Constants.kt
@@ -3,11 +3,10 @@ package com.tashila.hazle.utils
 import com.tashila.hazle.BuildConfig
 
 val SERVER_URL = if (BuildConfig.DEBUG)
-    "https://hazle-backend-dev.onrender.com/"
+    "https://hazle.up.railway.app/"
 else
     "https://api.hazle.tashila.me/"
 
 // "http://10.0.2.2:8080/" // For Android Emulator
 // "http://192.168.1.115:8080/" // Local deploy
-// "https://hazle.onrender.com/" // Render deploy
 // "https://api.hazle.tashila.me/" // Live deploy
